12724149d8f1bf2a1db84bc0f3984c975b75ac5c
vaglarov <pasific@abv.bg>
2019-09-12 19:11:01 +0300

    new

---
+++ Test 012019/05. Table-Relations/Exercises/04.SelfReference.sql
@@ -0,0 +1,14 @@
+--Problem04 SelfReferences
+CREATE TABLE Teachers
+(
+	TeacherID INT PRIMARY KEY IDENTITY(101,1)
+	,[NAME] CHAR(20)
+	,ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID) 
+)
+INSERT INTO Teachers([Name],ManagerID)
+VALUES  ('John'	,NULL)
+		,('Maya'	,106)
+		,('Silvia'	,106)
+		,('Ted'	,105)
+		,('Mark'	,101)
+		,('Greta'	,101)
